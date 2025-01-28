db0519f2d0ec0dbb9c33433ffab69eb0fa1feb74
rajmanna <raj2021manna@gmail.com>
2025-01-28 17:06:12 +0000

    Creation of SWARNA Coin

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,41 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+import Principal "mo:base/Principal";
+import Nat "mo:base/Nat";
+import Text "mo:base/Text";
+import HashMap "mo:base/HashMap";
+import Int "mo:base/Int";
+import Debug "mo:base/Debug";
+
+actor Token {
+    var owner : Principal = Principal.fromText("wuip4-5h3lr-lywfq-tbshz-twttx-bnmpk-pvkb2-bccak-goscy-gtoou-qqe");
+    var totalSupply : Nat = 1000000000;
+    var symbol: Text = "SWARNA";
+
+    var balances = HashMap.HashMap<Principal, Nat>(1, Principal.equal, Principal.hash);
+    balances.put(owner, totalSupply);
+
+    public query func balanceOf(who: Principal) : async Nat {
+
+        let balance : Nat = switch (balances.get(who)) {
+            case null 0;
+            case (?result) result;
+        };
+
+        return balance;
     };
-};
+
+    public query func getSymbol() : async Text {
+        return symbol;
+    };
+
+    public shared(msg) func payOut() : async Text {
+
+        if (balances.get(msg.caller)==null) {
+            // Debug.print(debug_show(msg.caller));
+            let amount = 10000;
+            balances.put(msg.caller, amount);
+            return "Success";
+        } else {
+            return "You already claimed!";
+        }
+    }
+}
---
+++ src/token_assets/src/components/Balance.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Principal } from '@dfinity/principal';
+import { token } from '../../../declarations/token';
+
+function Balance() {
+  const [inputValue, setInputValue] = useState('');
+  const [balanceResult, setBalanceResult] = useState('');
+  const [cryptoSymbol, setCryptoSymbol] = useState('');
+  const [isHidden, setIsHidden] = useState(true);
+
+  async function handleClick() {
+    const principal = Principal.fromText(inputValue);
+    const balance = await token.balanceOf(principal);
+    const symbol = await token.getSymbol();
+    setBalanceResult(balance.toLocaleString());
+    setCryptoSymbol(symbol);
+    setIsHidden(false);
+  }
+
+  return (
+    <div className="window white">
+      <label>Check account token balance:</label>
+      <p>
+        <input
+          id="balance-principal-id"
+          type="text"
+          placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+      </p>
+      <p className="trade-buttons">
+        <button id="btn-request-balance" onClick={handleClick}>
+          Check Balance
+        </button>
+      </p>
+      <p hidden={isHidden}>
+        This account has a balance of {balanceResult} {cryptoSymbol}.
+      </p>
+    </div>
+  );
+}
+
+export default Balance;
